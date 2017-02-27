61aa43c58145fa13093a76c6ee126491688201e0
jarbinup <jarbinup@qq.com>
2017-02-27 19:11:57 +0800

    change nodemon listen path

--- gruntfile.js
+++ gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function(grunt) {
 					args: [],
 					ignoredFiles: ['README.md', 'node_modules/**', 'DS_Store'],
 					watchedExtensions: ['js'],
-					watchedFloders: ['app', 'config'],
+					watchedFloders: ['./'],
 					debug: true,
 					delayTime: 1,
 					env: {
